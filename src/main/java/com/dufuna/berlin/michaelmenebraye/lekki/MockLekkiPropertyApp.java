4697ea379f9eddc77ef6d92f62e3eebdb1a573ea
MichaelMenebraye <michael@dufuna.org>
2023-10-06 14:04:56 +0100

    updated java fundamental 1 task

---
+++ src/main/java/com/dufuna/berlin/michaelmenebraye/lekki/MockLekkiPropertyApp.java
@@ -0,0 +1,32 @@
+package com.dufuna.berlin.michaelmenebraye.lekki;
+
+import com.dufuna.berlin.michaelmenebraye.lekki.model.LekkiProperty;
+import com.dufuna.berlin.michaelmenebraye.lekki.service.LekkiPropertyService;
+import com.dufuna.berlin.michaelmenebraye.lekki.service.MockLekkiPropertyService;
+
+public class MockLekkiPropertyApp {
+
+    public static void main(String[] args) {
+
+        LekkiProperty property = new LekkiProperty();
+        property.setPropertyId("Lekki001");
+        property.setAddress("Lekki phase 1 Street");
+        property.setPropertyType("bungalow");
+        property.setNumBedrooms(3);
+        property.setNumSittingRooms(2);
+        property.setNumKitchens(1);
+        property.setNumBathrooms(2);
+        property.setNumToilets(3);
+        property.setPropertyOwner("james brown");
+        property.setDescription("A beautiful house in 3 bedroom bungalow in Lekki");
+        property.setValidFrom("2023-10-01");
+        property.setValidTo("2023-12-31");
+
+
+        LekkiPropertyService propertyService = new MockLekkiPropertyService();
+
+        propertyService.saveProperty(property);
+
+        LekkiProperty retrievedProperty = propertyService.getProperty();
+    }
+}
--- src/main/java/com/dufuna/berlin/michaelmenebraye/lekki/service/MockLekkiPropertyService.java
+++ src/main/java/com/dufuna/berlin/michaelmenebraye/lekki/service/MockLekkiPropertyService.java
@@ -4,29 +4,6 @@ import com.dufuna.berlin.michaelmenebraye.lekki.model.LekkiProperty;
 
 public class MockLekkiPropertyService implements LekkiPropertyService {
 
-    public static void main(String[] args) {
-
-        LekkiProperty property = new LekkiProperty();
-        property.setPropertyId("Lekki001");
-        property.setAddress("Lekki phase 1 Street");
-        property.setPropertyType("bungalow");
-        property.setNumBedrooms(3);
-        property.setNumSittingRooms(2);
-        property.setNumKitchens(1);
-        property.setNumBathrooms(2);
-        property.setNumToilets(3);
-        property.setPropertyOwner("james brown");
-        property.setDescription("A beautiful house in 3 bedroom bungalow in Lekki");
-        property.setValidFrom("2023-10-01");
-        property.setValidTo("2023-12-31");
-
-
-        LekkiPropertyService propertyService = new MockLekkiPropertyService();
-
-        propertyService.saveProperty(property);
-
-        LekkiProperty retrievedProperty = propertyService.getProperty();
-    }
     @Override
     public void saveProperty(LekkiProperty property) {
         System.out.println("MockLekkiPropertyService.saveProperty called");
